2ad44be4cff3d45f3daefdd753ca5ac95befd3c7
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-02-17 13:28:45 -0500

    program update

--- main.ts
+++ main.ts
@@ -1,8 +1,21 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Matthew
+ * Created on: Feb 2026
+ * This program does basic math 
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(10)
+
+basic.showString('A rectangle has dimensions 5 cm & 3 cm.')
+basic.pause(10)
+
+/* In Perimeter */
+basic.showString('The perimeter would be:' + (2*(5+3)).toString())
+basic.pause(10)
+
+/* In Area */
+basic.showString('The area would be:' + (5*3).toString())
+basic.pause(10)
+
