c3c604307a0e698b9d30d250cbc9902582a85cb4
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-02-19 21:58:23 +0000

    updated main.py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,20 +5,20 @@ This module does basic math
 """
 
 from microbit import *
-from time import *
+from time import sleep
 
 
 display.clear()
-display.sleep(10)
+sleep(1)
 
 # given
 display.scroll("A rectangle has dimensions 5 cm & 3 cm.")
-display.sleep(10)
+sleep(1)
 
 # In Perimeter
-display.scroll("The perimeter would be:" + str(2*(5+3)))
-display.sleep(10)
+display.scroll("The perimeter would be:" + str(2 * (5 + 3)))
+sleep(1)
 
 # In Area
-display.scroll("The area would be:" + str(5*3))
-display.sleep(10)
+display.scroll("The area would be:" + str(5 * 3))
+sleep(1)
