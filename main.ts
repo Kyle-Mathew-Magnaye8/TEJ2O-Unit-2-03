3f0ee26e4227665ccaa96b6532171f68cd87f913
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-02-19 17:12:07 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -12,9 +12,9 @@ basic.showString('A rectangle has dimensions 5 cm & 3 cm.')
 basic.pause(10)
 
 /* In Perimeter */
-basic.showString('The perimeter would be:' + (2*(5+3)).toString())
+basic.showString('The perimeter would be:' + (2 * (5 + 3)).toString())
 basic.pause(10)
 
 /* In Area */
-basic.showString('The area would be:' + (5*3).toString())
+basic.showString('The area would be:' + (5 * 3).toString())
 basic.pause(10)
